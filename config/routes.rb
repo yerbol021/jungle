f0dc36cadca0612b7f210c89c5bb24d1772904a8
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-07-07 12:05:38 -0700

    Define the Routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,9 @@ Rails.application.routes.draw do
   resources :orders, only: [:create, :show]
 
   namespace :admin do
-    root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
