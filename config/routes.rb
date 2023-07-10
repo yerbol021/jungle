0b80811d8a9cff03c5f1f0dfd35f70a00974e8d2
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-07-09 18:40:44 -0700

    Add controller for registration and login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'users/new'
+  get 'users/create'
   get 'about', to: 'about#index'
   root to: 'products#index'
 
