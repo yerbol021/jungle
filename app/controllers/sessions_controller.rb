95161f677361b63edeaeca65267bd655ebf74c46
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-07-09 18:42:51 -0700

    Add logic to sessions conttroller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user&.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'Logged in!'
+    else
+      flash.now[:alert] = 'Invalid email/password combination'
+      render :new
+    end
   end
 
   def destroy
+    session.delete(:user_id)
+    redirect_to root_path, notice: 'Logged out!'
   end
 end
