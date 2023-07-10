5141cb6ebffd9351a351b65f6608047656647995
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-07-09 18:48:15 -0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'signup', to: 'users#new', as: 'signup'
+  post 'users', to: 'users#create'
+  get 'login', to: 'sessions#new', as: 'login'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
